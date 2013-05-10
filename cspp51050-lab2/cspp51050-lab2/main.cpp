29eda5e6d2c00ad72264d85f209c2703f56d56b7
Minsik Yu <minsik@uchicago.edu>
2013-05-09 20:21:43 -0500

    fasdfas

---
+++ cspp51050-lab2/cspp51050-lab2/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  cspp51050-lab2
+//
+//  Created by Min on 5/9/13.
+//  Copyright (c) 2013 cspp51050. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
